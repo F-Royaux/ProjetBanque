7272fc1a1286ac9223744d51595738e36f9a0326
franck <freville.franck@gmail.com>
2023-08-21 16:13:59 +0200

    changement du type de l'adresse, passage en string

--- banque/classAgence.php
+++ banque/classAgence.php
@@ -4,24 +4,8 @@ class Agence
 {
     // private ?int $CodeAgence;
     private string $NomAgence;
-    private array $Adresse;
-    private string $NumeroRueAdresse;
-    private int $CodePostalAgence;
-    private string $VilleAgence;
+    private string $Adresse;
 
-    public function pushAdresse()
-    {
-        $this->Adresse[] = $this->NumeroRueAdresse;
-        $this->Adresse[] = $this->CodePostalAgence;
-        $this->Adresse[] = $this->VilleAgence;
-        return $this;
-
-        //A tester
-        // $this->Adresse[] = readline("Saisir le numéro et la rue de l'agence : ");
-        // $this->Adresse[] = readline("Saisir le code postal de l'agence : ");
-        // $this->Adresse[] =  readline("Saisir la ville de l'agence : ");
-        // return $this;
-    }
     /**
      * Get the value of NomAgence
      *
@@ -51,7 +35,7 @@ class Agence
      *
      * @return string
      */
-    public function getAdresse(): array
+    public function getAdresse(): string
     {
         return $this->Adresse;
     }
@@ -63,9 +47,9 @@ class Agence
      *
      * @return self
      */
-    public function setAdresse(array $Adresse): self
+    public function setAdresse(): self
     {
-        $this->Adresse[] = $Adresse;
+        $this->Adresse = readline("Saisir l'adresse : ");
 
         return $this;
     }
@@ -75,94 +59,24 @@ class Agence
      *
      * @return ?int
      */
-    // public function getCodeAgence(): ?int
-    // {
-    //     return $this->CodeAgence;
-    // }
+    // // public function getCodeAgence(): ?int
+    // // {
+    // //     return $this->CodeAgence;
+    // // }
 
-    // /**
-    //  * Set the value of CodeAgence
-    //  *
-    //  * @param ?int $CodeAgence
-    //  *
-    //  * @return self
-    //  */
-    // public function setCodeAgence(?int $CodeAgence): self
-    // {
-    //     $this->CodeAgence = $CodeAgence;
+    // // /**
+    // //  * Set the value of CodeAgence
+    // //  *
+    // //  * @param ?int $CodeAgence
+    // //  *
+    // //  * @return self
+    // //  */
+    // // public function setCodeAgence(?int $CodeAgence): self
+    // // {
+    // //     $this->CodeAgence = $CodeAgence;
 
-    //     return $this;
-    // }
+    // //     return $this;
+    // // }
 
-    /**
-     * Get the value of NumeroRueAdresse
-     *
-     * @return string
-     */
-    public function getNumeroRueAdresse(): string
-    {
-        return $this->NumeroRueAdresse;
-    }
 
-    /**
-     * Set the value of NumeroRueAdresse
-     *
-     * @param string $NumeroRueAdresse
-     *
-     * @return self
-     */
-    public function setNumeroRueAdresse(): self
-    {
-        $this->NumeroRueAdresse = readline("Saisir le numéro et la rue de l'agence : ");
-
-        return $this;
-    }
-
-    /**
-     * Get the value of CodePostalAgence
-     *
-     * @return int
-     */
-    public function getCodePostalAgence(): int
-    {
-        return $this->CodePostalAgence;
-    }
-
-    /**
-     * Set the value of CodePostalAgence
-     *
-     * @param int $CodePostalAgence
-     *
-     * @return self
-     */
-    public function setCodePostalAgence(): self
-    {
-        $this->CodePostalAgence = readline("Saisir le code postal de l'agence : ");
-
-        return $this;
-    }
-
-    /**
-     * Get the value of VilleAgence
-     *
-     * @return string
-     */
-    public function getVilleAgence(): string
-    {
-        return $this->VilleAgence;
-    }
-
-    /**
-     * Set the value of VilleAgence
-     *
-     * @param string $VilleAgence
-     *
-     * @return self
-     */
-    public function setVilleAgence(): self
-    {
-        $this->VilleAgence = readline("Saisir la ville de l'agence : ");
-
-        return $this;
-    }
 }
--- banque/createfileagence.php
+++ banque/createfileagence.php
@@ -6,19 +6,25 @@ $numero = [];
 
 $agence = new Agence;
 $agence->setNomAgence();
-$agence->setNumeroRueAdresse();
-$agence->setCodePostalAgence();
-$agence->setVilleAgence();
-$agence->pushAdresse($numero);
-
+$agence->setAdresse();
+// $agence->setNumeroRueAdresse();
+// $agence->setCodePostalAgence();
+// $agence->setVilleAgence();
+// $agence->pushAdresse($numero);
 
 $fileName = "./sauv/agence.csv";
 $header = array("Nom", "Adresse");
 
 function createFile($client, $fileName, $header)
 {
-    $fp = fopen($fileName, "w");
-    fputcsv($fp, $header);
+
+    if (!file_exists($fileName)) {
+        $fp = fopen($fileName, "a");
+        fputcsv($fp, $header);
+        fclose($fp);
+    }
+    $fp = fopen($fileName, "a");
+
     fputcsv($fp, (array) $client);
     fclose($fp);
 }
