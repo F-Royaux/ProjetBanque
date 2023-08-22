8f31129c7b353a6dc30f5f318afe3e4c2cbad153
franck <freville.franck@gmail.com>
2023-08-22 08:51:32 +0200

    maj

---
+++ ressources/classAgenceversionmultiadresse.php
@@ -0,0 +1,92 @@
+// private string $NumeroRueAdresse;
+// private int $CodePostalAgence;
+// private string $VilleAgence;
+
+// public function pushAdresse()
+// {
+// $this->Adresse[] = $this->NumeroRueAdresse;
+// print_r($this->Adresse);
+// $this->Adresse[] = $this->CodePostalAgence;
+// print_r($this->Adresse);
+// $this->Adresse[] = $this->VilleAgence;
+// print_r($this->Adresse);
+
+// return $this->Adresse;
+
+// //A tester
+// // $this->Adresse[] = readline("Saisir le numéro et la rue de l'agence : ");
+// // $this->Adresse[] = readline("Saisir le code postal de l'agence : ");
+// // $this->Adresse[] = readline("Saisir la ville de l'agence : ");
+// // return $this;
+// }
+// /**
+// * Get the value of NumeroRueAdresse
+// *
+// * @return string
+// */
+// public function getNumeroRueAdresse(): string
+// {
+// return $this->NumeroRueAdresse;
+// }
+
+// /**
+// * Set the value of NumeroRueAdresse
+// *
+// * @param string $NumeroRueAdresse
+// *
+// * @return self
+// */
+// public function setNumeroRueAdresse(): self
+// {
+// $this->NumeroRueAdresse = readline("Saisir le numéro et la rue de l'agence : ");
+
+// return $this;
+// }
+
+// /**
+// * Get the value of CodePostalAgence
+// *
+// * @return int
+// */
+// public function getCodePostalAgence(): int
+// {
+// return $this->CodePostalAgence;
+// }
+
+// /**
+// * Set the value of CodePostalAgence
+// *
+// * @param int $CodePostalAgence
+// *
+// * @return self
+// */
+// public function setCodePostalAgence(): self
+// {
+// $this->CodePostalAgence = readline("Saisir le code postal de l'agence : ");
+
+// return $this;
+// }
+
+// /**
+// * Get the value of VilleAgence
+// *
+// * @return string
+// */
+// public function getVilleAgence(): string
+// {
+// return $this->VilleAgence;
+// }
+
+// /**
+// * Set the value of VilleAgence
+// *
+// * @param string $VilleAgence
+// *
+// * @return self
+// */
+// public function setVilleAgence(): self
+// {
+// $this->VilleAgence = readline("Saisir la ville de l'agence : ");
+
+// return $this;
+// }
